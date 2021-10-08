852c6bcdc70acedf26916c45c06bb0e1969054ac
viktor-dott <viktor@ridedott.com>
2021-10-08 13:48:29 +0200

    hello world and figuring out what is C

---
+++ day-one/dayOne.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void) {
+
+  int number;
+
+  printf("Please enter a number: ");
+  scanf("%d", &number);
+  printf("You entered %d\n", number);
+  getchar();
+  return 0;
+
+  int array[5];
+  
+
+}
